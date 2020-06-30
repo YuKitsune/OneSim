a16851df256250b32c9adc52af872bab8214a879
Eoin Motherway <eoinmoth@yahoo.ie>
2020-06-30 20:27:41 +1000

    Added migrations for DateTimeOffset
    Added a migration for the new DateTimeOffset properties in the Traffic API models

--- OneSim.Traffic.Persistence/Migrations/TrafficDb/20200630091220_ChangeDateTimeToDateTimeOffset.Designer.cs
+++ OneSim.Traffic.Persistence/Migrations/TrafficDb/20200630091220_ChangeDateTimeToDateTimeOffset.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OneSim.Traffic.Persistence;
@@ -9,9 +10,10 @@ using OneSim.Traffic.Persistence;
 namespace OneSim.Traffic.Persistence.Migrations.TrafficDb
 {
     [DbContext(typeof(TrafficDbContext))]
-    partial class StatusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200630091220_ChangeDateTimeToDateTimeOffset")]
+    partial class ChangeDateTimeToDateTimeOffset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OneSim.Traffic.Persistence/Migrations/TrafficDb/20200630091220_ChangeDateTimeToDateTimeOffset.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneSim.Traffic.Persistence.Migrations.TrafficDb
+{
+    public partial class ChangeDateTimeToDateTimeOffset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LogonTime",
+                table: "Pilots",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LogonTime",
+                table: "Controllers",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LogonTime",
+                table: "Pilots",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LogonTime",
+                table: "Controllers",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+        }
+    }
+}
