4aa6ae51db7c30a9148b3a8cddb828996b698723
Eoin Motherway <eoinmoth@yahoo.ie>
2020-07-04 15:29:33 +1000

    Updated HTTPS redirection to only be used in production

--- OneSim.Identity.Web/Startup.cs
+++ OneSim.Identity.Web/Startup.cs
@@ -113,9 +113,11 @@ namespace OneSim.Identity.Web
                 // The default HSTS value is 30 days.
                 // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+
+                // Redirect to HTTPS only
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
